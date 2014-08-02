bb1011730759b2d6a8e5670e5916f88c81cc7111
Artem Chivchalov <chivchalov@gmail.com>
2014-08-02 18:15:35 +0400

    Tests updated to angular versions 1.2.21 and 1.3.0-beta.17

--- README.md
+++ README.md
@@ -21,7 +21,7 @@ Or use this CDN link (thanks to [cdnjs.com](http://cdnjs.com)):
 <script src="//cdnjs.cloudflare.com/ajax/libs/angular-route-segment/1.3.0/angular-route-segment.min.js"></script>
 ```
 
-Tested with AngularJS 1.1.5, 1.2.16 and 1.3.0-beta.7.
+Tested with AngularJS 1.1.5, 1.2.21 and 1.3.0-beta.17.
 
 Overview
 --------
--- karma-angular-1.2.conf.js
+++ karma-angular-1.2.conf.js
@@ -8,10 +8,10 @@ module.exports = function(config) {
         files: [
             'test/lib/jquery.min.js',
             'test/lib/helpers.js',
-            'https://ajax.googleapis.com/ajax/libs/angularjs/1.2.16/angular.js',
-            'https://ajax.googleapis.com/ajax/libs/angularjs/1.2.16/angular-route.js',
-            'https://ajax.googleapis.com/ajax/libs/angularjs/1.2.16/angular-animate.js',
-            'https://ajax.googleapis.com/ajax/libs/angularjs/1.2.16/angular-mocks.js',
+            'https://ajax.googleapis.com/ajax/libs/angularjs/1.2.21/angular.js',
+            'https://ajax.googleapis.com/ajax/libs/angularjs/1.2.21/angular-route.js',
+            'https://ajax.googleapis.com/ajax/libs/angularjs/1.2.21/angular-animate.js',
+            'https://ajax.googleapis.com/ajax/libs/angularjs/1.2.21/angular-mocks.js',
             'src/**/*.js',
             'test/unit/**/*.js'
         ],
